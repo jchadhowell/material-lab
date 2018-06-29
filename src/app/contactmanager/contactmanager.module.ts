3158780340a41d583689227393ac8eab618718d0
Chad Howell <C_Howell@DellTeam.com>
2018-06-29 13:03:12 -0500

    added nav component

--- src/app/contactmanager/contactmanager.module.ts
+++ src/app/contactmanager/contactmanager.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactmanagerAppComponent } from './contactmanager-app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+
+import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '', component: ContactmanagerAppComponent,
+    children: [
+      { path: '', component: MainContentComponent }
+    ]},
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: [ContactmanagerAppComponent]
+  declarations: [ContactmanagerAppComponent, ToolbarComponent, MainContentComponent, SidenavComponent]
 })
 export class ContactmanagerModule { }
