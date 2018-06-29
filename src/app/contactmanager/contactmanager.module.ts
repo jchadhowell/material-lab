c057d589e5ab545d28b6b93fa6ac1c4124a16cbd
Chad Howell <C_Howell@DellTeam.com>
2018-06-29 13:11:35 -0500

    added main content

--- src/app/contactmanager/contactmanager.module.ts
+++ src/app/contactmanager/contactmanager.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+
 import { ContactmanagerAppComponent } from './contactmanager-app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 
-import { MaterialModule } from '../shared/material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-
 const routes: Routes = [
-  {
-    path: '', component: ContactmanagerAppComponent,
+  { path: '', component: ContactmanagerAppComponent,
     children: [
       { path: '', component: MainContentComponent }
-    ]},
+    ] },
   { path: '**', redirectTo: '' }
 ];
 
